a1283172ec13eb1558db6128b8f3df1c47f6cc37
James Andersson <18AnderJ@fulford.york.sch.uk>
2023-12-01 10:07:45 +0000

    changed status bar

--- Visualiser/Visualiser.Designer.cs
+++ Visualiser/Visualiser.Designer.cs
@@ -54,8 +54,8 @@
             // toolStripStatusLabel1
             // 
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(154, 17);
-            toolStripStatusLabel1.Text = "Click plot to display a graph";
+            toolStripStatusLabel1.Size = new Size(130, 17);
+            toolStripStatusLabel1.Text = "james plotter OH YEAH";
             // 
             // menuStrip1
             // 
